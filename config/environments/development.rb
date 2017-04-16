46de27afc50a3998d0a352cad838c0ce8d4184fe
thunderenlight <thunderenlight@gmail.com>
2017-04-16 13:44:06 -0500

    Adding paperclip config friendly for bucket format

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,11 +39,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_region: 'AWS_REGION',
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      bucket: 'S3_BUCKET_NAME',
+      access_key_id: 'AWS_ACCESS_KEY_ID',
+      secret_access_key: 'AWS_SECRET_ACCESS_KEY',
     }
   }
 
